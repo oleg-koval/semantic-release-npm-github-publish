7a69eb7e9eaec17ab6a25bbb54b5944dc2ca9550
Oleg Koval <kvl.olg@gmail.com>
2021-09-23 20:01:56 +0300

    chore(dependencies): fix vulnerabilities

--- release.config.js
+++ release.config.js
@@ -32,6 +32,7 @@ const customTransform = (commit, context) => {
 	}
 
 	if (typeof commit.hash === `string`) {
+		console.log("commit", commit.hash);
 		commit.shortHash = commit.hash.substring(0, 7);
 	}
 
@@ -63,6 +64,7 @@ const customTransform = (commit, context) => {
 	}
 
 	// remove references that already appear in the subject
+	console.log("🚀 ~ file: release.config.js ~ line 74 ~ customTransform ~ commit.references", commit.references)
 	commit.references = commit.references.filter(reference => {
 		if (issues.indexOf(reference.issue) === -1) {
 			return true;
@@ -70,7 +72,7 @@ const customTransform = (commit, context) => {
 
 		return false;
 	});
-
+	console.log("🚀 ~ file: release.config.js ~ line 75 ~ customTransform ~ commit.references", commit.references)
 	return commit;
 };
 
